d5f7260e3e21a92ab962b645a547e939309ef809
Piyush Kumar <k17piyush@gmail.com>
2026-07-08 04:24:54 +0530

    PCP-7027: mask existing containerd and kubelet to make sure it doesn't block snap services

--- pkg/cloudinit/scripts/disable-host-services.sh
+++ pkg/cloudinit/scripts/disable-host-services.sh
@@ -9,4 +9,6 @@
 for svc in kubelet containerd; do
   systemctl stop "${svc}" || true
   systemctl disable "${svc}" || true
+  ln -sf /dev/null "/etc/systemd/system/${svc}.service"
 done
+systemctl daemon-reload
